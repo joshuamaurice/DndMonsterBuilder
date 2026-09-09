6ba08482db2ee94677bfa501a2dc2fd203b91702
Joshua Maurice <jmaurice@internal.salesforce.com>
2026-09-09 08:43:38 -0700

    Adding stat #2 for sorted joined for some common stats like immunities2, weaknesses2, etc.

--- src/main/java/jmaurice/dnd/stats/builder/raw/basic/MiscBasics.java
+++ src/main/java/jmaurice/dnd/stats/builder/raw/basic/MiscBasics.java
@@ -9,21 +9,60 @@ public class MiscBasics extends BaseBuilder {
     public MiscBasics(final Stats stats) { super(stats); }
 
     public void build() {
-        agg("alignment", rootleaf); 
-        agg("challenge rating", leaf, values -> sumAsInts(values));
-        agg("creature subtypes", leaf, values -> join(sort(values), ", "));
-        agg("defensive abilities", leaf, values -> join(sort(values), ", "));
-        agg("equipment", rootleaf, values -> join(sort(values), ", "));
+        agg("alignment", root); 
+        agg("challenge rating", values -> sumAsInts(values));
+        
+        aggN("creature subtype", root, values -> values); 
+        toN("creature subtypes", "creature subtype");
+        aggN("creature subtypes", values -> sort(values));
+        agg("creature subtypes2", leaf, values -> join(sort(values), ", "));
+        toN("creature subtypes2", "creature subtypes", values -> values);
+        
+        aggN("defensive abilities", values -> sort(values));
+        agg("defensive abilities2", leaf, values -> join(sort(values), ", "));
+        toN("defensive abilities2", "defensive abilities", values -> values);
+        
+        aggN("equipment", root, values -> sort(values));
+        agg("equipment2", leaf, values -> join(sort(values), ", "));
+        toN("equipment2", "equipment", values -> values);
+        
+        agg("gender", root);
         agg("high attack DC", leaf, values -> maxAsInts(values));
-        agg("immunities", leaf, values -> join(sort(values), ", "));
-        agg("incorporeal", rootleaf, values -> values.size() >= 1 ? new Value(1) : null);
-        agg("initiative", leaf, values -> sumAsInts(values));
+        
+        aggN("immunities", values -> sort(values));
+        agg("immunities2", leaf, values -> join(sort(values), ", "));
+        toN("immunities2", "immunities", values -> values);
+        
+        agg("initiative", values -> withSign(sumAsInts(values)));
         to1("initiative", "dexterity modifier");
+        to1("initiative", "default", new Value(0));
+        
+        agg("name", root);
         agg("psionic manifester level", values -> sumAsInts(values));
-        agg("senses", leaf, values -> join(sort(values), ", "));
-        agg("weaknesses", rootleaf, values -> join(sort(values), ", ")); //TODO remove root
-        agg("special abilities long", leaf, values -> join(sort(values), "__NEWLINE__"));
-        agg("special abilities short", rootleaf, values -> join(sort(values), ", ")); //TODO remove root
+        agg("race", root);
+        
+        aggN("resistances", values -> sort(values));
+        agg("resistances2", leaf, values -> join(sort(values), ", "));
+        toN("resistances2", "resistances", values -> values);
+        
+        aggN("senses", values -> sort(values));
+        agg("senses2", leaf, values -> join(sort(values), ", "));
+        toN("senses2", "senses", values -> values);
+        
+        aggN("special abilities long", values -> sort(values));
+        agg("special abilities long2", leaf, values -> join(sort(values), ", "));
+        toN("special abilities long2", "special abilities long", values -> values);
+        
+        aggN("special abilities short", values -> sort(values));
+        agg("special abilities short2", leaf, values -> join(sort(values), ", "));
+        toN("special abilities short2", "special abilities short", values -> values);
+        
+        aggN("templates", root, values -> sort(values));
+        
+        aggN("weaknesses", root, values -> sort(values));
+        agg("weaknesses2", leaf, values -> join(sort(values), ", "));
+        toN("weaknesses2", "weaknesses", values -> values);
+        
     }
 
 }
