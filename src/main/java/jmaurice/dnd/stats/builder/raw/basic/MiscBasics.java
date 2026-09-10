e671e23bb40e30eac52ee4b5dba2f90dd4bf4777
Joshua Maurice <jmaurice@internal.salesforce.com>
2026-09-09 19:23:51 -0700

    removing various stat-name2 stats

--- src/main/java/jmaurice/dnd/stats/builder/raw/basic/MiscBasics.java
+++ src/main/java/jmaurice/dnd/stats/builder/raw/basic/MiscBasics.java
@@ -11,58 +11,30 @@ public class MiscBasics extends BaseBuilder {
     public void build() {
         agg("alignment", root); 
         agg("challenge rating", values -> sumAsInts(values));
-        
-        aggN("creature subtype", root, values -> values); 
-        toN("creature subtypes", "creature subtype");
+        aggN("auras", root, values -> sort(values));
         aggN("creature subtypes", values -> sort(values));
-        agg("creature subtypes2", leaf, values -> join(sort(values), ", "));
-        toN("creature subtypes2", "creature subtypes", values -> values);
-        
+        aggN("damage reduction", root, values -> sort(values));
         aggN("defensive abilities", values -> sort(values));
-        agg("defensive abilities2", leaf, values -> join(sort(values), ", "));
-        toN("defensive abilities2", "defensive abilities", values -> values);
-        
         aggN("equipment", root, values -> sort(values));
-        agg("equipment2", leaf, values -> join(sort(values), ", "));
-        toN("equipment2", "equipment", values -> values);
-        
         agg("gender", root);
         agg("high attack DC", leaf, values -> maxAsInts(values));
-        
         aggN("immunities", values -> sort(values));
-        agg("immunities2", leaf, values -> join(sort(values), ", "));
-        toN("immunities2", "immunities", values -> values);
         
         agg("initiative", values -> withSign(sumAsInts(values)));
         to1("initiative", "dexterity modifier");
         to1("initiative", "default", new Value(0));
         
+        aggN("languages", root, values -> sort(values));
         agg("name", root);
         agg("psionic manifester level", values -> sumAsInts(values));
         agg("race", root);
-        
         aggN("resistances", values -> sort(values));
-        agg("resistances2", leaf, values -> join(sort(values), ", "));
-        toN("resistances2", "resistances", values -> values);
-        
         aggN("senses", values -> sort(values));
-        agg("senses2", leaf, values -> join(sort(values), ", "));
-        toN("senses2", "senses", values -> values);
-        
+        agg("shape", root);
         aggN("special abilities long", values -> sort(values));
-        agg("special abilities long2", leaf, values -> join(sort(values), ", "));
-        toN("special abilities long2", "special abilities long", values -> values);
-        
         aggN("special abilities short", values -> sort(values));
-        agg("special abilities short2", leaf, values -> join(sort(values), ", "));
-        toN("special abilities short2", "special abilities short", values -> values);
-        
         aggN("templates", root, values -> sort(values));
-        
         aggN("weaknesses", root, values -> sort(values));
-        agg("weaknesses2", leaf, values -> join(sort(values), ", "));
-        toN("weaknesses2", "weaknesses", values -> values);
-        
     }
 
 }
